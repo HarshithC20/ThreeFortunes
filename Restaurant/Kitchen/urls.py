5636fd65d9e8ccd1eb6cf5d5c9efff25374c802d
harshithc20 <harshithc20@gmail.com>
2024-12-06 23:45:25 +0530

    Removed the orders and kitchen app. Modified the accounts to add the food items in html file

--- Restaurant/Kitchen/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import kitchen
-
-urlpatterns = [
-    path('kitchen/', kitchen, name='Kitchen View')
-]
--- Restaurant/orders/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def orders(request):
-    return HttpResponse("Welcome to Orders page")
